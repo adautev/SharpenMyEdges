9eefbc361c0776fab49be4c84adc699fb6549eed
adautev <andrei@dautev.eu>
2018-03-02 11:16:48 +0200

    Stack with resizing array. (:

--- src/StacksAndQueues/Main.java
+++ src/StacksAndQueues/Main.java
@@ -8,27 +8,45 @@ public class Main {
 
     public static void main(String[] args) throws Exception {
 
-        StackLL<String> stack = new StackLL<String>();
-        stack.Push("backwards!");
-        stack.Push("forward");
+        StackLL<String> stackLL = new StackLL<String>();
+        stackLL.Push("backwards!");
+        stackLL.Push("forward");
         //Ooops!
-        stack.Pop();
-        stack.Push("spelled ");
-        stack.Push("are");
+        stackLL.Pop();
+        stackLL.Push("spelled ");
+        stackLL.Push("are");
         //Oops!
-        stack.Pop();
-        stack.Push("is ");
-        stack.Push("sentence ");
-        stack.Push("This ");
+        stackLL.Pop();
+        stackLL.Push("is ");
+        stackLL.Push("sentence ");
+        stackLL.Push("This ");
 
         StringBuilder stackLLresult = new StringBuilder();
-        while (!stack.isEmpty()) {
-            stackLLresult.append(stack.Pop());
+        while (!stackLL.isEmpty()) {
+            stackLLresult.append(stackLL.Pop());
         }
         if(!stackLLresult.toString().trim().equals("This sentence is spelled backwards!")) {
             throw new Exception("You couldn't manage ONE stack!");
         }
-
+        StackAR<String> stackAR = new StackAR<String>();
+        stackAR.Push("backwards!");
+        stackAR.Push("forward");
+        //Ooops!
+        stackAR.Pop();
+        stackAR.Push("spelled ");
+        stackAR.Push("are");
+        //Oops!
+        stackAR.Pop();
+        stackAR.Push("is ");
+        stackAR.Push("sentence ");
+        stackAR.Push("This ");
+        StringBuilder stackARresult = new StringBuilder();
+        while (!stackAR.isEmpty()) {
+            stackARresult.append(stackAR.Pop());
+        }
+        if(!stackARresult.toString().trim().equals("This sentence is spelled backwards!")) {
+            throw new Exception("You couldn't manage ONE stack!");
+        }
         System.out.println("Stacks and queues tests completed successfully.");
     }
 
---
+++ src/StacksAndQueues/StackAR.java
@@ -0,0 +1,45 @@
+package StacksAndQueues;
+
+import java.lang.reflect.Array;
+
+public class StackAR<T> {
+    private T[] elementsContainer = (T[]) new Object[1];
+    private int elementsCounter;
+    public StackAR() {
+    }
+
+    public boolean isEmpty() {
+        return this.elementsCounter == 0;
+    }
+    public void Push(T value) {
+        if(this.elementsContainer.length == (this.elementsCounter+1)) {
+            T[] newContainer = copyToNewArray(this.elementsContainer.length * 2);
+            this.elementsContainer = newContainer;
+        }
+        this.elementsContainer[this.elementsCounter] = value;
+        this.elementsCounter++;
+    }
+
+    private T[] copyToNewArray(int newArrayLength) {
+        T[] newContainer = (T[]) new Object[newArrayLength];
+        for (int i = 0; i < this.elementsContainer.length && newContainer.length > i; i++) {
+            newContainer[i] = this.elementsContainer[i];
+        }
+        return newContainer;
+    }
+
+    public T Pop() {
+
+        if(isEmpty()) {
+            throw new IndexOutOfBoundsException("No more mr. Nice Guy!");
+        }
+        this.elementsCounter--;
+        T returnValue = this.elementsContainer[this.elementsCounter];
+        this.elementsContainer[this.elementsCounter] = null;
+        if(this.elementsCounter <= (this.elementsContainer.length / 4)) {
+            T[] newContainer = copyToNewArray(this.elementsContainer.length / 2);
+            this.elementsContainer = newContainer;
+        }
+        return returnValue;
+    }
+}
